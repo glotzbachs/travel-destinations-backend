8a40c9fbdbed349529c69f61247ccd4f603c727b
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2020-03-12 23:11:20 -0400

    set up controllers

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,2 +1,42 @@
 class AttractionsController < ApplicationController
+
+    def index
+        @attractions = Attraction.all
+        render json: @attractions
+    end
+
+    def show
+        @attraction = Attraction.find_by(id: params[:id])
+        render json: @attraction
+    end
+
+    def create
+        @attraction = Attraction.new(attraction_params)
+        if @attraction.save
+            render json: @attraction
+        else
+            render json: {error: 'Attraction cannot be created'}
+        end
+    end
+
+    def update
+        if @attraction.update(attraction_params)
+            render json: @attraction
+        else
+            render json: @attraction.errors
+        end
+    end
+
+    def destroy
+        @attraction = Attraction.find_by(id: params[:id])
+        @attraction.destroy
+    end
+
+    private
+
+    def attraction_params
+        params.require(:attraction).permit(:name, :description, :destination_id)
+    end
+
+
 end
--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,2 +1,40 @@
 class DestinationsController < ApplicationController
+
+    def index
+        @destinations = Destination.all
+        render json: @destinations
+    end
+
+    def create
+        @destination = Destination.new(destination_params)
+        if @destination.save
+            render json: @destination
+        else
+            render json: {error: 'Destination cannot be created'}
+        end
+    end
+
+    def show
+        @destination = Destination.find_by(id: params[:id])
+        render json: @destination
+    end
+
+    def update
+        if @destination.update(destination_params)
+            render json: @destination
+        else
+            render json: @destination.errors
+        end
+    end
+
+    def destroy
+        @destination = Destination.find_by(id: params[:id])
+        @destination.destroy
+    end
+
+    private
+
+    def destination_params
+        params.require(:destination).permit(:name, :location, :description)
+    end
 end
